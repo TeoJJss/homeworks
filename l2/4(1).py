78bb7d0419096269ddf1ac96919865fbee94d455
TeoJJss <teojunjia@gmail.com>
2023-03-28 19:49:25 +0800

    n

---
+++ l2/4(1).py
@@ -0,0 +1,8 @@
+name=input("Name: ")
+tp=input("TP Number: ")
+mark=int(input("Mark: "))
+grade=input("Grade: ")
+
+print(name, tp, mark, grade)
+
+# print(input("Name: "), input("TP Number: "), int(input("Mark: ")), input("Grade: "))
