d899d4994035ab7d5a7d5cbb910a29ebbfb7d922
TeoJJss <teojunjia@gmail.com>
2023-03-31 16:28:48 +0800

    modify 1c/C/2

--- lab1/1c/C/2.py
+++ lab1/1c/C/2.py
@@ -1,32 +1,32 @@
+'''
+If one person is vegetarian, the restaurants must be vegetarian
+If nobody is vegetarian, the restaurants can be vegetarian or non-vegetarian
+'''
+
 inp1=input("Is anyone is a vegetarian? ")
 inp2=input("Is anyone is a vegan? ")
 inp3=input("Is anyone is a gluten-free? ")
 
-vege=True if inp1=="yes" else False
-vegan=True if inp2=="yes" else False
-gluten=True if inp3=="yes" else False
+vege=True if inp1.lower().strip()=="yes" else False
+vegan=True if inp2.lower().strip()=="yes" else False
+gluten=True if inp3.lower().strip()=="yes" else False
 
-if vege and vegan and gluten: # If all are yes
-    print("Corner Cafe")
-    print("The Chef's Kitchen")
+rest=["Joe's Gourmet Burgers", "Main Street Pizza Company", "Corner Cafe",
+    "Mama's Fine Italian", "The Chef's Kitchen"] # A list of restaurants can go
 
-elif not vege and not vegan and not gluten: # All no
-    print("Joe Gourmet Burgers")
-    print("Corner Cafe")
-    print("The Chef's Kitchen")
-    print("Main Street Pizza Company")
-    print("Mama's Fine Italian")
-elif not vege and not vegan and gluten: # have gluten-free
-    print("Main Street Pizza Company\nCorner Cafe\nThe Chef's Kitchen")
-elif not vege and vegan and not gluten: # have vegan
-    print("Corner Cafe")
-    print("The Chef's Kitchen")
-elif not vege and vegan and gluten: # have vegan and gluten-free
-    print("Corner Cafe")
-    print("The Chef's Kitchen")
-elif vege and not vegan and not gluten: # have vege
-    print("Main Street Pizza Company\nCorner Cafe\nMama's Fine Italian\nThe Chef's Kitchen")
-elif vege and not vegan and gluten: # have vege and gluten-free
-    print("Main Street Pizza Company")
-elif vege and vegan and not gluten:  # have vege and vegan
-    print("Mama's Fine Italian")
+if vege: # If got ppl vege, restaurants must "Yes" with vege
+    if "Joe's Gourmet Burgers" in rest: rest.remove("Joe's Gourmet Burgers") # Remove restaurants with "No" vege
+
+if vegan: # If got ppl vegan, restaurants must "Yes" with vegan
+    # Remove restaurants with "No" vegan
+    if "Joe's Gourmet Burgers" in rest: rest.remove("Joe's Gourmet Burgers")
+    rest.remove("Main Street Pizza Company")
+    if "Mama's Fine Italian" in rest: rest.remove("Mama's Fine Italian")
+
+if gluten: # If got ppl gluten-free, restaurants must "Yes" with gluten-free
+    # Remove restaurants with "No" vegan
+    if "Joe's Gourmet Burgers" in rest: rest.remove("Joe's Gourmet Burgers")
+    if "Mama's Fine Italian" in rest: rest.remove("Mama's Fine Italian")
+
+
+print("\n".join(rest))
